e4cd56a9c6c855ec2961ce70058fdd570353748a
vcuende <victor.morcuende.castell@alumnos.upm.es>
2023-01-13 19:38:50 +0100

    Final app (1st version)

--- app.R
+++ app.R
@@ -30,7 +30,7 @@ df <- read_csv("data/mxmh_survey_results.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  navbarPage("Music X Mental Health",
+  navbarPage("Music x Mental Health",
              id="tabs",
 
     tabPanel("Correlations",
@@ -38,7 +38,9 @@ ui <- fluidPage(
                headerPanel("Relationships between two variables"),
                tags$div(style = "margin-top: 20px", sidebarPanel(
                  tags$h3("Correlation between numeric variables"),
+                 br(),
                  tags$h4("Choose the variables you wish to study"),
+                 br(),
                  # Input for selecting the two variables to compare
                  selectInput(inputId = "var1", label = "Variable 1", choices = names(df)[sapply(df, is.numeric)]),
                  selectInput(inputId = "var2", label = "Variable 2", choices = names(df)[sapply(df, is.numeric)]),
@@ -52,9 +54,9 @@ ui <- fluidPage(
              )
     ),
     
-    tabPanel("BarChart",
+    tabPanel("Bar Chart",
              pageWithSidebar(
-               headerPanel("Relationships between Mental Illnesses and subject\'s age and hours spent"),
+               headerPanel("Relationships between Mental Illnesses and subject\'s favorite music genre"),
                tags$div(style = "margin-top: 20px", sidebarPanel(
                  checkboxGroupInput("varsBarChart", "Mental Illnesses to be seen in the bar chart:",
                                     c("Anxiety" = "Anxiety", "Depression" = "Depression", "Insomnia" = "Insomnia", "OCD" = "OCD"),
@@ -66,31 +68,34 @@ ui <- fluidPage(
                )
              )
     ),
-
-    tabPanel("LineChart",
+    
+    tabPanel("Line Chart",
              pageWithSidebar(
                headerPanel("Evolution of Mental Illnesses based on subject\'s age"),
                tags$div(style = "margin-top: 20px", sidebarPanel(
+                 checkboxGroupInput("lineChartOption", "Method to summarise the data:",
+                                    c("Sum" = "Sum", "Mean" = "Mean"),
+                                    selected = c("Sum", "Mean")),
                  checkboxGroupInput("varsLineChart", "Mental Illnesses to include in the evolution line chart:",
                                     c("Anxiety" = "Anxiety", "Depression" = "Depression", "Insomnia" = "Insomnia", "OCD" = "OCD"),
                                     selected = c("Anxiety")))
                ),
-              mainPanel(
-                # Output for displaying the line chart of the selected variables
-                plotOutput("plotLineChart")
-              )
+               mainPanel(
+                 # Output for displaying the line chart of the selected variables
+                 plotOutput("plotLineChart")
+               )
              )
     ),
     
-    tabPanel("HeatMap",
+    tabPanel("Heat Map",
              pageWithSidebar(
-               headerPanel("Relationships between two variables"),
+               headerPanel("Distribution of Mental Illnesses in subjects"),
                tags$div(style = "margin-top: 20px", sidebarPanel(
                  checkboxInput("check", "Create Heatmap with Favorite Genre and different Mental Illnesses"),
                  conditionalPanel(
                    condition = "input.check == false",
                    # Input for selecting the two variables to compare
-                   selectInput(inputId = "var3", label = "Categorical Variable", choices = names(df)[!sapply(df, is.numeric)]),
+                   selectInput(inputId = "var3", label = "Categorical Variable", choices = names(df)[!sapply(df, is.numeric) & !(names(df) %in% "Timestamp")]),
                    selectInput(inputId = "var4", label = "Variable to aggregate 1", choices = names(df)[sapply(df, is.numeric)]),
                    selectInput(inputId = "var5", label = "Variable to aggregate 2", choices = names(df)[sapply(df, is.numeric)]),
                    selectInput(inputId = "var6", label = "Variable to aggregate 3", choices = names(df)[sapply(df, is.numeric)]),
@@ -116,6 +121,7 @@ server <- function(input, output) {
   df <- df[rowSums(is.na(df)) == 0,]
 
   observeEvent(input$button1, {
+    
     col1 <- df[, input$var1]
     col2 <- df[, input$var2]
 
@@ -123,7 +129,6 @@ server <- function(input, output) {
     print(is.numeric(as.data.frame(col2)[[1]]))
     print(class(as.data.frame(col1)[[1]]))
     print(class(as.data.frame(col2)[[1]]))
-
 
     if(!is.numeric(as.data.frame(col1)[[1]]) && is.numeric(as.data.frame(col2)[[1]])){
       print("Boxplot of col2 grouped by col1")
@@ -147,72 +152,140 @@ server <- function(input, output) {
       print("Correlation between col1 and col2")
       output$correlation <- renderText({
         # Return the value as a character string
-        return(paste("Correlation between", input$var2, "and", input$var1, ":", as.character(cor_val)))
+        return(paste("Correlation between", input$var1, "and", input$var2, ":", as.character(cor_val)))
       })
       output$corplot <- renderPlotly({
         print("Scatter plot of col1 vs col2")
         ggplot(data = df, aes(x = unlist(col2), y = unlist(col1))) +
           geom_point() +
-          labs(title = "Scatter plot of col1 vs col2", x = input$var2,  y = input$var1)
+          xlab(input$var2) +
+          ylab(input$var1)
       })
     }
+    
   })
   
   
   observeEvent(input$varsBarChart, {
     
+    df_sum <- df %>%
+      group_by(`Fav genre`) %>%
+      summarise(Anxiety = sum(Anxiety),
+                Depression = sum(Depression),
+                Insomnia = sum(Insomnia),
+                OCD = sum(OCD))
+
     filtered_data <- reactive({
-      df[, c("Fav genre", input$varsLineChart)]
+      df_sum[, c("Fav genre", input$varsBarChart)]
     })
+
+    filtered_data_long <- filtered_data() %>%
+      select(`Fav genre`, input$varsBarChart) %>%
+      gather(key = "Legend", value = "Computed Illness Degree", -`Fav genre`)
+    
+    filtered_data_ord <- filtered_data_long %>%
+      arrange(desc(`Computed Illness Degree`))
     
     output$plotBarChart <- renderPlot({
-      ggplot(df, aes(x = `Fav genre`, y = Anxiety)) +
-        geom_bar(stat = "identity", fill = "blue") +
-        ggtitle("Mental Illnesses by Fav genre") +
-        xlab("Fav genre") +
-        ylab("Count")
+      ggplot(filtered_data_ord, aes(x = reorder(`Fav genre`, -`Computed Illness Degree`), y = `Computed Illness Degree`, fill = Legend)) +
+        geom_bar(stat = "identity", position = "stack") +
+        xlab("Fav genre")
     })
-      
-    # filtered_data <- reactive({
-    #   selected_illnesses <- input$varsBarChart
-    #   df %>% filter(Anxiety %in% selected_illnesses | Depression %in% selected_illnesses | Insomnia %in% selected_illnesses | OCD %in% selected_illnesses)
-    # })
-    # 
-    # output$plotBarChart <- renderPlot({
-    #   ggplot(filtered_data(), aes(x = `Fav genre`, y = count)) +
-    #     geom_bar(stat = "identity", fill = "blue") +
-    #     ggtitle("Mental Illnesses by Fav genre") +
-    #     xlab("Fav genre") +
-    #     ylab("Count")
-    # })
     
   })
   
+  
   observeEvent(input$varsLineChart, {
-    
-    df_mean <- df %>%
-      group_by(Age) %>%
-      summarise(Anxiety = mean(Anxiety),
-                Depression = mean(Depression),
-                Insomnia = mean(Insomnia),
-                OCD = mean(OCD))
-    
-    filtered_data <- reactive({
-      df_mean[, c("Age", input$varsLineChart)]
+    observeEvent(input$lineChartOption, {
+      
+      if("Sum" %in% input$lineChartOption & "Mean" %in% input$lineChartOption){
+        print("Both checks pressed")
+        
+        df_sum <- df %>%
+          group_by(Age) %>%
+          summarise(Anxiety = sum(Anxiety),
+                    Depression = sum(Depression),
+                    Insomnia = sum(Insomnia),
+                    OCD = sum(OCD))
+        
+        df_mean <- df %>%
+          group_by(Age) %>%
+          summarise(Anxiety = mean(Anxiety),
+                    Depression = mean(Depression),
+                    Insomnia = mean(Insomnia),
+                    OCD = mean(OCD))
+        
+        filtered_data_sum <- reactive({
+          df_sum[, c("Age", input$varsLineChart)]
+        })
+        
+        filtered_data_mean <- reactive({
+          df_mean[, c("Age", input$varsLineChart)]
+        })
+        
+        filtered_data_long_mean <- filtered_data_mean() %>% 
+          select(Age, input$varsLineChart) %>% 
+          gather(key = "Legend", value = "Computed Illness Degree", -Age)
+        
+        filtered_data_long_sum <- filtered_data_sum() %>% 
+          select(Age, input$varsLineChart) %>% 
+          gather(key = "Legend", value = "Computed Illness Degree", -Age)
+        
+        output$plotLineChart <- renderPlot({
+          plots <- list()
+          
+          plots[[1]] <- ggplot(filtered_data_long_sum, aes(x = Age, y = `Computed Illness Degree`, color = Legend)) +
+            geom_line(size = 1) + geom_point(size = 2) + stat_summary(fun.y = mean, geom = "line", aes(group = 1)) +
+            labs(title = "Sum method")
+          
+          plots[[2]] <- ggplot(filtered_data_long_mean, aes(x = Age, y = `Computed Illness Degree`, color = Legend)) +
+            geom_line(size = 1) + geom_point(size = 2) + stat_summary(fun.y = mean, geom = "line", aes(group = 1)) +
+            labs(title = "Mean method")
+          
+          gridExtra::grid.arrange(grobs = plots)
+        })
+      }
+      else if("Sum" %in% input$lineChartOption | "Mean" %in% input$lineChartOption){
+        print("One check pressed")
+        if("Sum" %in% input$lineChartOption){
+          print("Sum check pressed")
+          df_filtered <- df %>%
+            group_by(Age) %>%
+            summarise(Anxiety = sum(Anxiety),
+                      Depression = sum(Depression),
+                      Insomnia = sum(Insomnia),
+                      OCD = sum(OCD))
+        }
+        if("Mean" %in% input$lineChartOption){
+          print("Mean check pressed")
+          df_filtered <- df %>%
+            group_by(Age) %>%
+            summarise(Anxiety = mean(Anxiety),
+                      Depression = mean(Depression),
+                      Insomnia = mean(Insomnia),
+                      OCD = mean(OCD))
+        }
+        
+        filtered_data <- reactive({
+          df_filtered[, c("Age", input$varsLineChart)]
+        })
+        
+        filtered_data_long <- filtered_data() %>% 
+          select(Age, input$varsLineChart) %>% 
+          gather(key = "Legend", value = "Computed Illness Degree", -Age)
+        
+        output$plotLineChart <- renderPlot({
+          ggplot(filtered_data_long, aes(x = Age, y = `Computed Illness Degree`, color = Legend)) +
+            geom_line(size = 1) + geom_point(size = 2) + stat_summary(fun.y = mean, geom = "line", aes(group = 1))
+        })
+      }
+      
     })
-    filtered_data_long <- filtered_data() %>% 
-      select(Age, input$varsLineChart) %>% 
-      gather(key = "Legend", value = "Computed Illness Degree", -Age)
-    
-    output$plotLineChart <- renderPlot({
-      ggplot(filtered_data_long, aes(x = Age, y = `Computed Illness Degree`, color = Legend)) +
-        geom_line(size = 1.5) + geom_point(size = 2.5) + stat_summary(fun.y = mean, geom = "line", aes(group = 1))
-    })
-    
   })
   
   
   observeEvent(input$button3, {
+    
     if(isTRUE(input$check)){
       df2 <- df %>% select("Fav genre", "Anxiety", "Depression", "Insomnia", "OCD")
       print("Aggregating variables")
@@ -235,7 +308,9 @@ server <- function(input, output) {
         heatmaply(df_agg, labCol = col_labels)
       })
     }
+    
   })
+  
 }
 
 # Run the application
